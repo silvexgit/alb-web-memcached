9d4e23532f8bad5013fb8af29ae6a9d54d8efde8
silvexgit <31254668+silvexgit@users.noreply.github.com>
2023-09-20 22:03:09 -0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -7,7 +7,8 @@ output "Memcache_Hostname" {
   value = "${aws_elasticache_cluster.silvex-memcached.configuration_endpoint}"
 }
 
-#output "silvex-centos-8-101-tf" {
-#  value = aws_instance.silvex-centos-8-101-tf.public_dns
+#output "silvex-aws-linux-101-tf" {
+#  value = aws_instance.silvex-aws-linux-101-tf.public_dns
 #}
+
 
