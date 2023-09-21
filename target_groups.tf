06f833abcf0e4f0c7150f811f401781fc8011524
Ed Silva <esilva@silvex.com>
2023-09-20 22:40:23 -0700

    Updates

--- target_groups.tf
+++ target_groups.tf
@@ -10,16 +10,16 @@ resource "aws_lb_target_group" "silvex-tg-172-33-web" {
 
 resource "aws_lb_target_group_attachment" "silvex-tg-ec2-172-33-1" {
   target_group_arn = aws_lb_target_group.silvex-tg-172-33-web.arn
-  target_id        = aws_instance.silvex-centos-8-100-tf.id
+  target_id        = aws_instance.silvex-aws-linux-100-tf.id
   port             = 80
 }
 resource "aws_lb_target_group_attachment" "silvex-tg-ec2-172-33-2" {
   target_group_arn = aws_lb_target_group.silvex-tg-172-33-web.arn
-  target_id        = aws_instance.silvex-centos-8-101-tf.id
+  target_id        = aws_instance.silvex-aws-linux-101-tf.id
   port             = 80
 }
 resource "aws_lb_target_group_attachment" "silvex-tg-ec2-172-33-3" {
   target_group_arn = aws_lb_target_group.silvex-tg-172-33-web.arn
-  target_id        = aws_instance.silvex-centos-8-102-tf.id
+  target_id        = aws_instance.silvex-aws-linux-102-tf.id
   port             = 80
 }
--- vpc.tf
+++ vpc.tf
@@ -4,8 +4,8 @@ resource "aws_vpc" "silvex-vpc-172-33" {
   instance_tenancy     = "default"
   enable_dns_support   = "true"
   enable_dns_hostnames = "true"
-  enable_classiclink   = "false"
   tags = {
     Name = "silvex-vpc-172-33"
   }
 }
+
