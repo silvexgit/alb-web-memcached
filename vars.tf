02b0f8566fb8a6a42dbb2de56b5ceead1e17a9f8
silvexgit <31254668+silvexgit@users.noreply.github.com>
2023-09-20 22:02:23 -0700

    Update vars.tf with multiple regions.

--- vars.tf
+++ vars.tf
@@ -2,12 +2,24 @@ variable "SILVEX-REGION" {
   default = "us-west-2"
 }
 
+variable "SILVEX-REGION-A" {
+  default = "us-west-2a"
+}
+
+variable "SILVEX-REGION-B" {
+  default = "us-west-2b"
+}
+
+variable "SILVEX-REGION-C" {
+  default = "us-west-2c"
+}
+
 variable "SILVEX-SSH-2020" {
-  default = "silvex-ssh-key-2020-july-01-1344"
+  default = "silvex-ed25519-west2-2023-sep-20"
 }
 
 variable "SILVEX-AMI" {
-  default = "ami-0800fc0fa715fdcfe"
+  default = "ami-0f3769c8d8429942f"
 }
 
 variable "SILVEX-EC2-TYPE" {
